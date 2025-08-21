7d82742a6616fb5dada603e505368f6010302553
Mateo <cuellamateo@gmail.com>
2025-08-21 13:51:06 -0300

    feat: completar actividad 3

---
+++ src/03.ts
@@ -0,0 +1,13 @@
+// Intefaz básica
+
+interface Person {
+  firstNmae: string
+  lastName: string
+}
+
+const person: Person = {
+  firstNmae: 'Mateo',
+  lastName: 'Cuella Fleitas',
+}
+
+console.log(person)
